a750f91dad3c5b4eeff25d88af6fbbf40831096c
Taylor Ho <tayho@twitch.tv>
2017-06-28 18:00:51 -0700

    added hears `interactive`

--- skills/sample_hears.js
+++ skills/sample_hears.js
@@ -56,6 +56,33 @@ module.exports = function(controller) {
         }
     });
 
+    controller.hears('interactive', 'direct_message', function(bot, message) {
+
+        bot.reply(message, {
+            attachments:[
+                {
+                    title: 'Do you want to interact with my buttons?',
+                    callback_id: '123',
+                    attachment_type: 'default',
+                    actions: [
+                        {
+                            "name":"yes",
+                            "text": "Yes",
+                            "value": "yes",
+                            "type": "button",
+                        },
+                        {
+                            "name":"no",
+                            "text": "No",
+                            "value": "no",
+                            "type": "button",
+                        }
+                    ]
+                }
+            ]
+        });
+    });
+
 
     /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
     /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
